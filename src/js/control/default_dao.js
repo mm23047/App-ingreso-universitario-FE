1c7fb41237d20932c996901680784eb91e4f7ecd
Jeffry Uziel Aguilar Morales <am23014@clases.ues.sv>
2026-05-28 13:56:23 -0600

    Actualización de archivos modificados

--- src/js/control/default_dao.js
+++ src/js/control/default_dao.js
@@ -1,7 +1,10 @@
 class DefaultDao {
     constructor() {
         this.BASE_URL = 'http://localhost:9080/IngresoUniversitarioTPI135-1.0-SNAPSHOT/resources/v1/';
-    
+    }
+
+    getBaseUrl() {
+        return this.BASE_URL;
     }
 }
 
